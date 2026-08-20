f8fd7afe94c1ed58762e72fc55d8e34a0aa8ce4f
Misha21111 <mishatsertsvadze1990@gmail.com>
2026-08-20 15:12:51 +0200

    Fix indentation for st.markdown call

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-  st.markdown(
+    st.markdown(
         f"""
         <div class="donut-container">
             <div class="donut-ring" style="background: conic-gradient(#36A2EB 0deg {p_deg}deg, #FFCE56 {p_deg}deg {f_deg}deg, #FF6384 {f_deg}deg {c_deg}deg);">
@@ -15,5 +15,5 @@
             </div>
         </div>
         """,
-        unsafe_allow_html=True # <--- ДОДАЙТЕ ЦЕ
+        unsafe_allow_html=True
     )
